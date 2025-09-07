34bcd5dccaabc16e751c6666054e545e5b9c427d
Shuvarup66 <shuvarup26@gmail.com>
2025-09-07 13:05:48 +0530

    added mode

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ question }),
+        mode: "cors",
       });
       const data = await res.json();
 
